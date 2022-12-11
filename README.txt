d88acb1d941d60fa6d62130c4aa910f6bd75edea
justinkacher <justinkacher@gmail.com>
2022-12-11 12:52:51 -0500

    update file saving

--- README.txt
+++ README.txt
@@ -1,3 +1,5 @@
 record battery voltage output vs time with constant current draw
 http://www.testequipmentdepot.com/keithley/pdfs/2450_manual.pdf 
 http://www.testequipmentdepot.com/keithley/pdfs/2450_datasheet.pdf 
+
+Run at 4  3.5  3  2.5  2  1.5  1 Amps
--- datacollection.py
+++ datacollection.py
@@ -1,27 +1,51 @@
-from locale import currency
-from re import I
 import pandas as pd
 import time
-import Keithley2450_wrapper.main as keithley
+import Keithley2450_wrapper.main as keithley    #also initializes connection to keithley
 
-curr_load = input("current load: ")
+
+#Make sure to change TCP_IP in main.py to match keithley address
+#initialization vars
+use_keithley_load = False   #T-uses keithley as current source F-uses external curr load to be set manually
+current_load = 2            #in amps
+#where the excel dataframe will be saved
+path = ""       #no path will save in folder of program  
+
 batteryID = input("scan battery or enter battery ID num: ")
+timearr = []
+voltagearr = []
+curr_loadarr = []
+powerarr = []
+datadict = {"Time":timearr,"Voltage (V)":voltagearr,"Current Load (A)":curr_loadarr,"Watts (W)":powerarr}
+df = pd.DataFrame(data = datadict)
+df.to_excel(path+"CC_discharge_"+str(batteryID)+".xlsx")
 
-start_time = time.time
-keithley.current_output_on(curr_load)
-i=0
-while voltage[i-1] > 1:
-    timearr[i] = time.time-start_time
-    voltagearr[i] = keithley.get_voltage(20)
-    real_currarr[i] = keithley.get_current(1.05)
-    powerarr[i] = voltagearr[i]*real_curr[i]
-    curr_loadarr[i] = curr_load
+def savedata():
+    df = pd.DataFrame(data = datadict)
+    with pd.ExcelWriter(path+"CC_discharge_"+str(batteryID)+".xlsx",mode="a",engine="openpyxl",if_sheet_exists="overlay") as writer:
+        df.to_excel(writer, sheet_name="Sheet1",header=None, startrow=writer.sheets["Sheet1"].max_row,index=False)
+
+
+
+start_time = time.time()
+
+
+
+if use_keithley_load:
+    keithley.current_output_on(current_load)
+i = 0
+while voltage[i-1] > 2:
+    timearr.append(time.time()-start_time)
+    voltagearr.append(keithley.get_voltage(20))
+    curr_loadarr.append(current_load)
     i+=1
-    time.sleep(10)
+    savedata()
+    time.sleep(5)
 
-dict = {"Time":timearr,"Voltage (V)":voltagearr,"Current Load (A)":curr_loadarr,"Real Current (A)":real_currarr,"Watts (W)":powerarr}
-df = pd.DataFrame(data = dict)
-df.to_excel(str(batteryID))
+
+
+
+
+
 
 
 
